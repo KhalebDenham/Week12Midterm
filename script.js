4280132d78db0b5ee2446db6524e244bc77e4203
Khaleb Denham <60304297+KhalebDenham@users.noreply.github.com>
2025-06-13 12:34:13 -0500

    Update 2

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 const API_SHORTCUT = ""; //API shortcut for URL
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Get function
 $(document).ready(function () {
   const getUsers = () => {
     console.log("Getting all users from API");
     return $.get(`${API_SHORTCUT}/user`);
   };
 
+  //Function that posts a new user
+  const postUser = (event) => {
+    event.preventDefault();
+    //create an object
+    const userObject = {
+      name: $("#fullName"),
+      jobTitle: $("#jobTitle"),
+      companyName: $("#companyName"),
+    };
+    console.log(userObject);
+  };
+
+  //Delete User Method
+  const deleteUser = (id) => {
+    //feed the
+    console.log("Deleting user...");
+    $.ajax({
+      type: "DELETE",
+      url: `${API_SHORTCUT}/user/${id}`,
+      success: getUsers().done(renderData()),
+    });
+  };
+
   const renderData = () => {
     //takes the data from getUsers and logs the data
 
+    //Get function
     getUsers().then((data) => {
       for (let user of users) {
         //Prepends by Iterating each user in the array of users and adding their information to the table with id tBody from html file
@@ -33,10 +41,20 @@ $(document).ready(function () {
                     <td>${user.name}</td>
                     <td>${user.jobTitle}</td>
                     <td>${user.companyName}</td>
-                    <td><button class="btn btn-danger">Delete User</button></td>
+                    <td><button id="delete ${user.id}" class="btn btn-danger">Delete User</button></td>
                 </tr>
-                    `);
+       
+       
+       
+                `);
+        //Add an event listener for every delete button..pass in their user id so we can use that user.id as target in our function
+        $(`#delete${user.id}`).click(() => deleteUser(user.id));
       }
     });
   };
+
+  //Event Listener
+  $("#formSubmit").click(postUser);
+
+  renderData();
 });
